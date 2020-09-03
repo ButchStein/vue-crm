d73cf5240bd9aa2eab02484eaff562e7172f20f4
BBS <butchbs1982@gmail.com>
2020-09-03 14:18:45 +0300

    правки в названиях и отображении пунктов сайдбара

--- src/components/app/Sidebar.vue
+++ src/components/app/Sidebar.vue
@@ -20,9 +20,9 @@ export default {
   props: ['value'],
   data: () => ({
     links: [
-      { title: 'Счет', url: '/', exact: true },
+      { title: 'Счета', url: '/', exact: true },
       { title: 'История', url: '/history' },
-      { title: 'Планирование', url: '/planning' },
+      // { title: 'Планирование', url: '/planning' },
       { title: 'Новая запись', url: '/record' },
       { title: 'Категории', url: '/categories' }
     ]
